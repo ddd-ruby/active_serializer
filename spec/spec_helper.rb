afac205b81b8ad83285dc3a8e65f4ad1cb199dcd
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-05 21:55:29 +0100

    adds code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'byebug'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/.direnv/"
+end
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'active_serializer'
 
 RSpec.configure do |config|
